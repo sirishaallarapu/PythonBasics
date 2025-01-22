73997560c3b70c96bc491cdcfaaa4180d782aade
sirishaallarapu <allarapusirisha@gmail.com>
2025-01-22 13:50:03 +0530

    Add files via upload

---
+++ Day-1/Datatypes.py
@@ -0,0 +1,28 @@
+x= "Hello World"
+print(type(x))
+
+y= 123
+print(type(y))
+
+z= "123"
+print(type(z))
+
+a= 12.5
+print(type(a))
+
+b = ("ball","bat","wicket")
+print(type(b))
+
+b = ["ball","bat","wicket"]
+print(type(b))
+
+#datatype conversions
+
+x = "123"
+print(int(x))
+
+a= '123'
+b= '456'
+print(a+b)
+print(int(a)+int(b))
+
